56d0edd086e4004b3f07770690b313027d19cbd1
jujube565 <julien.meunier@usherbrooke.ca>
2015-12-11 15:55:58 -0500

    Removing toast and debugs

--- Client/app/src/main/java/ca/usherbrooke/koopa/udesvolumecontrol/VolumeEntry.java
+++ Client/app/src/main/java/ca/usherbrooke/koopa/udesvolumecontrol/VolumeEntry.java
@@ -14,36 +14,10 @@ public class VolumeEntry
 {
     private Integer  ringtoneVolume;
     private Integer  notificationVolume;
-
-    public Location getLocation()
-    {
-        return location;
-    }
-
-    public int getRadius()
-    {
-        return radius;
-    }
-
     private Location location;
     private int radius;
     private String entryName;
 
-    public Integer getRingtoneVolume()
-    {
-        return ringtoneVolume;
-    }
-
-    public Integer getNotificationVolume()
-    {
-        return notificationVolume;
-    }
-
-    /*
-            @param loc : location
-            @param rad : radius in meter
-
-         */
     public VolumeEntry(String name, Location loc, int rad,int ringtone, int notification)
     {
         entryName = name;
@@ -56,13 +30,31 @@ public class VolumeEntry
     public boolean isInside(Location otherLocation, Context baseContext)
     {
         float locationDist = location.distanceTo(otherLocation);
-        Toast.makeText(baseContext, entryName + " at " + locationDist, Toast.LENGTH_SHORT).show();
-        Log.v("Location check", entryName + " at Lat " + location.getLatitude() + " long " +  location.getLongitude());
-        Log.v("Location check", "VS new at Lat " + otherLocation.getLatitude() + " long " + otherLocation.getLongitude() + " distance = " + locationDist);
-
         return locationDist < radius;
     }
 
+    public Location getLocation()
+    {
+        return location;
+    }
 
-    public String getEntryName() { return entryName; }
+    public int getRadius()
+    {
+        return radius;
+    }
+
+    public String getEntryName()
+    {
+        return entryName;
+    }
+
+    public Integer getRingtoneVolume()
+    {
+        return ringtoneVolume;
+    }
+
+    public Integer getNotificationVolume()
+    {
+        return notificationVolume;
+    }
 }
