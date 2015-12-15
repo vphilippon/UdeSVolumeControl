18fd06fe51410338f70e9341ef362ab1d4d2b91e
unknown <phiv2201@DINF-0014-39A.DInf.FSCI.USherbrooke.ca>
2015-12-15 09:59:20 -0500

    Rename Config - VolumeConfig

---
+++ Server/Scripts/VolumeConfig.sql
@@ -0,0 +1,21 @@
+﻿-- Table: "VolumeConfig"
+
+-- DROP TABLE "VolumeConfig";
+
+CREATE TABLE "VolumeConfig"
+(
+  "id" serial NOT NULL,
+  "name" character varying(50) NOT NULL,
+  zone circle NOT NULL,
+  "profile" integer NOT NULL,
+  "userId" character varying(50) NOT NULL,
+  CONSTRAINT pk_config PRIMARY KEY ("id"),
+  CONSTRAINT fk_user_volumeconfig FOREIGN KEY ("userId")
+      REFERENCES "User" ("userId") MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "VolumeConfig"
+  OWNER TO ift604;
